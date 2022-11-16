adf593c872b6f048737352a83fdc5cd714a4c4f9
WaithakaFM <franco313mwas@gmail.com>
2022-11-16 22:00:09 +0300

    First commit

---
+++ app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
@@ -0,0 +1,10 @@
+package com.example.busschedule
+
+import android.app.Application
+import com.example.busschedule.database.schedule.AppDatabase
+
+class BusScheduleApplication : Application() {
+    val database: AppDatabase by lazy {
+        AppDatabase.getDatabase(this)
+    }
+}
---
+++ app/src/main/java/com/example/busschedule/database/schedule/AppDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.busschedule.database.schedule
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Schedule::class), version = 1)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun scheduleDao(): ScheduleDao
+
+    //    ensure that only one instance of the database exists to prevent race conditions or other
+    //    potential issues.
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "app_database")
+                    .createFromAsset("database/bus_schedule.db")
+                    .build()
+                INSTANCE = instance
+
+                instance
+            }
+        }
+    }
+}
